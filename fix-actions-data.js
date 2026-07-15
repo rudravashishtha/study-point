57d067cb4acd99221464fcd0c97342811b2d2589
rudravashishtha <rudraturtle7@gmail.com>
2026-07-15 16:24:04 +0530

    chore: remove temp files

--- fix-actions-data.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let websiteClient = fs.readFileSync('src/app/admin/website/components/WebsiteContentClient.tsx', 'utf8');
-
-websiteClient = websiteClient.replace(/updateTestimonialAction\(editing.id, data\)/, 'updateTestimonialAction(editing.id, { ...data, studentClass: data.studentClass as ClassLevel | undefined })');
-websiteClient = websiteClient.replace(/createTestimonialAction\(data\)/, 'createTestimonialAction({ ...data, studentClass: data.studentClass as ClassLevel | undefined })');
-
-fs.writeFileSync('src/app/admin/website/components/WebsiteContentClient.tsx', websiteClient);
